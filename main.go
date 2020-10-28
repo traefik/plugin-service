8a8e6ede34be7aaf888ea30042ae193864ea9daa
Trois-Six <trois.six@free.fr>
2020-10-28 12:50:22 +0100

    feat: add CLI and tracing

--- main.go
+++ main.go
@@ -1,115 +1,146 @@
 package main
 
 import (
-	"flag"
-	"net/http"
 	"os"
 
-	"github.com/fauna/faunadb-go/v3/faunadb"
 	"github.com/rs/zerolog/log"
-	"github.com/traefik/plugin-service/pkg/db"
-	"github.com/traefik/plugin-service/pkg/functions"
-	"github.com/traefik/plugin-service/pkg/logger"
+	"github.com/traefik/plugin-service/cmd/dbinit"
+	"github.com/traefik/token-service/cmd/serve"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	logger.Setup()
-
-	host := flag.String("host", "0.0.0.0:8080", "listening port and hostname")
-	secret := flag.String("secret", os.Getenv("FAUNADB_SECRET"), "Secret for database access.")
-	endpoint := flag.String("endpoint", os.Getenv("FAUNADB_ENDPOINT"), "Endpoint for database access.")
-	help := flag.Bool("h", false, "show this help")
-
-	flag.Usage = usage
-	flag.Parse()
-	if *help {
-		usage()
+	app := &cli.App{
+		Name:  "Plugin CLI",
+		Usage: "Run plugin service",
+		Commands: []*cli.Command{
+			dbInitCommand(),
+			serveCommand(),
+		},
 	}
 
-	nArgs := flag.NArg()
-	if nArgs > 0 {
-		usage()
-	}
-
-	if secret == nil || *secret == "" {
-		log.Fatal().Msg("FaunaDB secret is required.")
-	}
-
-	var options []faunadb.ClientConfig
-	if endpoint != nil && *endpoint != "" {
-		if err := os.Setenv("FAUNADB_ENDPOINT", *endpoint); err != nil {
-			log.Fatal().Err(err).Msg("Unable to set FAUNADB_ENDPOINT")
-		}
-		options = append(options, faunadb.Endpoint(*endpoint))
-	}
-
-	token, err := initDB(*secret, options)
+	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Error while bootstraping")
-	}
-
-	if err = os.Setenv("FAUNADB_SECRET", token); err != nil {
-		log.Fatal().Err(err).Msg("Unable to set FAUNADB_SECRET")
-	}
-
-	if err = bootstrap(token, options); err != nil {
-		log.Fatal().Err(err).Msg("Error while bootstraping")
-	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/public/", functions.Public)
-	mux.HandleFunc("/internal/", functions.Internal)
-	mux.HandleFunc("/external/", functions.External)
-
-	err = http.ListenAndServe(*host, mux)
-	if err != nil {
-		log.Fatal().Err(err).Msg("Error in http server")
+		log.Fatal().Err(err).Msg("Error while executing command")
 	}
 }
 
-func usage() {
-	_, _ = os.Stderr.WriteString("\t plugin-service \n")
-	flag.PrintDefaults()
-	os.Exit(2)
-}
-
-func initDB(secret string, options []faunadb.ClientConfig) (string, error) {
-	dbName := "plugin"
-
-	adminClient := faunadb.NewFaunaClient(secret, options...)
-	result, err := adminClient.Query(faunadb.Exists(faunadb.Database(dbName)))
-	if err != nil {
-		return "", err
+func dbInitCommand() *cli.Command {
+	cmd := &cli.Command{
+		Name:        "dbinit",
+		Usage:       "Init database",
+		Description: "Init production database",
+		Action:      dbinit.Run,
 	}
 
-	if !getExist(result) {
-		_, err = adminClient.Query(faunadb.CreateDatabase(faunadb.Obj{"name": dbName}))
-		if err != nil {
-			return "", err
-		}
+	cmd.Flags = append(cmd.Flags, sharedFlags()...)
+
+	return cmd
+}
+
+func serveCommand() *cli.Command {
+	cmd := &cli.Command{
+		Name:        "serve",
+		Usage:       "Serve HTTP",
+		Description: "Launch plugin service application",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "host",
+				Usage:   "Host to listen on.",
+				EnvVars: []string{"PILOT_HOST"},
+			},
+			&cli.StringFlag{
+				Name:     "token-url",
+				Usage:    "Token Service URL",
+				EnvVars:  []string{"PILOT_TOKEN_URL"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "jwt-cert",
+				Usage:    "Pilot JWT Cert",
+				EnvVars:  []string{"PILOT_JWT_CERT"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "services-access-token",
+				Usage:    "Pilot Services Access Token",
+				EnvVars:  []string{"PILOT_SERVICES_ACCESS_TOKEN"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "go-proxy-url",
+				Usage:    "Pilot Go Proxy URL",
+				EnvVars:  []string{"PILOT_GO_PROXY_URL"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "go-proxy-username",
+				Usage:    "Pilot Go Proxy Username",
+				EnvVars:  []string{"PILOT_GO_PROXY_USERNAME"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "go-proxy-password",
+				Usage:    "Pilot Go Proxy Password",
+				EnvVars:  []string{"PILOT_GO_PROXY_PASSWORD"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "github-token",
+				Usage:    "Pilot GitHub Token",
+				EnvVars:  []string{"PILOT_GITHUB_TOKEN"},
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "tracing-endpoint",
+				Usage:    "Endpoint to send traces",
+				EnvVars:  []string{"TRACING_ENDPOINT"},
+				Value:    "https://collector.infra.traefiklabs.tech",
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:     "tracing-username",
+				Usage:    "Username to connect to Jaeger",
+				EnvVars:  []string{"TRACING_USERNAME"},
+				Value:    "jaeger",
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:     "tracing-password",
+				Usage:    "Password to connect to Jaeger",
+				EnvVars:  []string{"TRACING_PASSWORD"},
+				Value:    "jaeger",
+				Required: false,
+			},
+			&cli.Float64Flag{
+				Name:     "tracing-probability",
+				Usage:    "Probability to send traces.",
+				EnvVars:  []string{"TRACING_PROBABILITY"},
+				Value:    0,
+				Required: false,
+			},
+		},
+		Action: serve.Run,
 	}
 
-	key, err := adminClient.Query(
-		faunadb.CreateKey(faunadb.Obj{
-			"database": faunadb.Database(dbName),
-			"role":     "server",
-		}),
-	)
+	cmd.Flags = append(cmd.Flags, sharedFlags()...)
 
-	return getSecret(key), err
+	return cmd
 }
 
-func getSecret(key faunadb.Value) (secret string) {
-	_ = key.At(faunadb.ObjKey("secret")).Get(&secret)
-	return
-}
-
-func getExist(key faunadb.Value) (exist bool) {
-	_ = key.Get(&exist)
-	return
-}
-
-func bootstrap(token string, options []faunadb.ClientConfig) error {
-	database := db.NewFaunaDB(faunadb.NewFaunaClient(token, options...))
-	return database.Bootstrap()
+func sharedFlags() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:     "faunadb-secret",
+			Usage:    "FaunaDB secret",
+			EnvVars:  []string{"FAUNADB_SECRET"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "faunadb-endpoint",
+			Usage:    "FaunaDB endpoint",
+			EnvVars:  []string{"FAUNADB_ENDPOINT"},
+			Required: false,
+		},
+	}
 }
