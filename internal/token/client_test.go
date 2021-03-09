be8d5e18c07219f73bf9103a70049faf8c43f7e6
Harold Ozouf <harold.ozouf@gmail.com>
2021-03-09 14:42:22 +0100

    feat: fake token-service

--- internal/token/client_test.go
+++ internal/token/client_test.go
@@ -2,100 +2,21 @@ package token
 
 import (
 	"context"
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-	"time"
 
-	guuid "github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func mock(data interface{}, status int) *httptest.Server {
-	return httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(status)
-			err := json.NewEncoder(w).Encode(data)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-		}))
-}
-
 func TestClientGet(t *testing.T) {
-	token := &Token{
-		ID:        guuid.New().String(),
-		Value:     "valueAAA",
-		Name:      "NameAAA",
-		CreatedAt: time.Time{},
-	}
+	client := Client{}
 
-	tokenSrv := mock(token, http.StatusOK)
-	defer tokenSrv.Close()
-
-	client := New(tokenSrv.URL, "")
-
-	checked, err := client.Get(context.Background(), token.ID)
+	_, err := client.Get(context.Background(), "valueAAA")
 	require.NoError(t, err)
-
-	assert.Equal(t, token, checked)
 }
 
 func TestClientCheck(t *testing.T) {
-	token := &Token{
-		ID:        guuid.New().String(),
-		Value:     "valueAAA",
-		Name:      "NameAAA",
-		CreatedAt: time.Time{},
-	}
+	client := Client{}
 
-	tokenSrv := mock(token, http.StatusOK)
-	defer tokenSrv.Close()
-
-	client := New(tokenSrv.URL, "")
-
-	checked, err := client.Check(context.Background(), token.Value)
+	_, err := client.Check(context.Background(), "valueAAA")
 	require.NoError(t, err)
-
-	assert.Equal(t, token, checked)
-}
-
-func TestClientCheck_errors(t *testing.T) {
-	testCases := []struct {
-		desc         string
-		token        string
-		data         Token
-		serverStatus int
-		expected     int
-	}{
-		{
-			desc:  "empty token",
-			token: "",
-			data: Token{
-				ID:        guuid.New().String(),
-				Value:     "valueAAA",
-				Name:      "NameAAA",
-				CreatedAt: time.Time{},
-			},
-			serverStatus: http.StatusInternalServerError,
-		},
-	}
-
-	for _, test := range testCases {
-		tokenSrv := mock(test.data, test.serverStatus)
-
-		test := test
-		t.Run(test.desc, func(t *testing.T) {
-			client := New(tokenSrv.URL, "")
-
-			checked, err := client.Check(context.Background(), test.token)
-			assert.Error(t, err)
-
-			require.Nil(t, checked)
-		})
-
-		tokenSrv.Close()
-	}
 }
