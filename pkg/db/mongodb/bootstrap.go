62627c3edd5dc765dd789b92a3266435aad3b452
Harold Ozouf <harold.ozouf@gmail.com>
2021-03-12 15:16:39 +0100

    feat: add Create method for MongoDB

---
+++ pkg/db/mongodb/bootstrap.go
@@ -0,0 +1,44 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// MongoIndex represents a MongoDB index.
+type MongoIndex struct {
+	Key  bson.D
+	Name string
+	V    int32
+}
+
+// Bootstrap indexes if not present (collection is automatically created).
+func (m *MongoDB) Bootstrap() error {
+	models := []mongo.IndexModel{
+		{
+			Options: &options.IndexOptions{
+				Name:   stringPtr("_uniq_id"),
+				Unique: boolPtr(true),
+			},
+			Keys: bson.D{{Key: "id", Value: 1}},
+		},
+	}
+
+	if _, err := m.client.Collection(m.collName).Indexes().CreateMany(context.Background(), models); err != nil {
+		return fmt.Errorf("unable to create indexes: %w", err)
+	}
+
+	return nil
+}
+
+func stringPtr(val string) *string {
+	return &val
+}
+
+func boolPtr(val bool) *bool {
+	return &val
+}
---
+++ pkg/db/mongodb/plugindb.go
@@ -0,0 +1,111 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"github.com/traefik/plugin-service/pkg/db"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const collName = "token"
+
+// MongoDB is a mongoDB client.
+type MongoDB struct {
+	client   *mongo.Database
+	collName string
+	tracer   trace.Tracer
+}
+
+// NewMongoDB creates a MongoDB.
+func NewMongoDB(client *mongo.Database) *MongoDB {
+	return &MongoDB{
+		client:   client,
+		collName: collName,
+		tracer:   otel.Tracer("Database"),
+	}
+}
+
+type pluginDocument struct {
+	db.Plugin `bson:",inline"`
+
+	MongoID primitive.ObjectID `bson:"_id,omitempty"`
+	Hashes  []db.PluginHash
+}
+
+// Get returns the plugin corresponding to the given ID.
+func (m *MongoDB) Get(ctx context.Context, id string) (db.Plugin, error) {
+	panic("implement me")
+}
+
+// Delete deletes the plugin corresponding to the given ID.
+func (m *MongoDB) Delete(ctx context.Context, id string) error {
+	panic("implement me")
+}
+
+// Create creates a new plugin.
+func (m *MongoDB) Create(ctx context.Context, plugin db.Plugin) (db.Plugin, error) {
+	ctx, span := m.tracer.Start(ctx, "db_create")
+	defer span.End()
+
+	id := primitive.NewObjectID()
+	plugin.ID = id.Hex()
+	plugin.CreatedAt = time.Now().Truncate(time.Millisecond)
+
+	doc := pluginDocument{
+		Plugin:  plugin,
+		MongoID: id,
+	}
+
+	_, err := m.client.Collection(m.collName).InsertOne(ctx, doc)
+	if err != nil {
+		span.RecordError(err)
+
+		return db.Plugin{}, err
+	}
+
+	return plugin, nil
+}
+
+// List lists plugins.
+func (m *MongoDB) List(ctx context.Context, pagination db.Pagination) ([]db.Plugin, string, error) {
+	panic("implement me")
+}
+
+// GetByName gets the plugin with the given name.
+func (m *MongoDB) GetByName(ctx context.Context, s string) (db.Plugin, error) {
+	panic("implement me")
+}
+
+// SearchByName searches for plugins matching with the given name.
+func (m *MongoDB) SearchByName(ctx context.Context, s string, pagination db.Pagination) ([]db.Plugin, string, error) {
+	panic("implement me")
+}
+
+// Update updates the given plugin.
+func (m *MongoDB) Update(ctx context.Context, id string, plugin db.Plugin) (db.Plugin, error) {
+	panic("implement me")
+}
+
+// DeleteHash deletes a plugin hash.
+func (m *MongoDB) DeleteHash(ctx context.Context, id string) error {
+	panic("implement me")
+}
+
+// CreateHash creates a new plugin hash.
+func (m *MongoDB) CreateHash(ctx context.Context, module, version, hash string) (db.PluginHash, error) {
+	panic("implement me")
+}
+
+// GetHashByName returns the hash corresponding the given name.
+func (m *MongoDB) GetHashByName(ctx context.Context, module, version string) (db.PluginHash, error) {
+	panic("implement me")
+}
+
+// Ping pings MongoDB to check it health status.
+func (m *MongoDB) Ping(ctx context.Context) error {
+	return m.client.Client().Ping(ctx, nil)
+}
