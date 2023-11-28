a4cdd0b848d424950d928ac671e0082cb04afa6a
Ludovic Fernandez <ldez@users.noreply.github.com>
2023-11-28 14:58:11 +0100

    feat: support WASM plugins

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -7,6 +7,8 @@ type Plugin struct {
 	ID            string                 `json:"id,omitempty" bson:"id"`
 	Name          string                 `json:"name,omitempty" bson:"name"`
 	DisplayName   string                 `json:"displayName,omitempty" bson:"displayName"`
+	Runtime       string                 `json:"runtime,omitempty" bson:"runtime"`
+	WasmPath      string                 `json:"wasmPath,omitempty" bson:"wasmPath"`
 	Author        string                 `json:"author,omitempty" bson:"author"`
 	Type          string                 `json:"type,omitempty" bson:"type"`
 	Import        string                 `json:"import,omitempty" bson:"import"`
@@ -23,7 +25,7 @@ type Plugin struct {
 	Disabled      bool                   `json:"disabled,omitempty" bson:"disabled"`
 }
 
-// PluginHash The plugin hash tuple..
+// PluginHash The plugin hash tuple.
 type PluginHash struct {
 	Name string `json:"name,omitempty" bson:"name"`
 	Hash string `json:"hash,omitempty" bson:"hash"`
