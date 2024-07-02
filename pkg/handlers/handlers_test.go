c7568291e2c56db11ef9d34417b96abd8fe58feb
Michael <mmatur@users.noreply.github.com>
2024-07-02 16:04:03 +0200

    feat: handle hidden plugin
    Co-authored-by: Julien Salleyron <julien.salleyron@gmail.com>

--- pkg/handlers/handlers_test.go
+++ pkg/handlers/handlers_test.go
@@ -89,7 +89,7 @@ func TestHandlers_List_GetByName(t *testing.T) {
 	}
 
 	testDB := mockDB{
-		getByNameFn: func(_ context.Context, _ string) (db.Plugin, error) {
+		getByNameFn: func(_ context.Context, _ string, _ bool) (db.Plugin, error) {
 			return data, nil
 		},
 	}
@@ -103,6 +103,56 @@ func TestHandlers_List_GetByName(t *testing.T) {
 	assert.Equal(t, http.StatusOK, rw.Code)
 
 	file, err := os.ReadFile("./fixtures/get_plugin_by_name.json")
+	require.NoError(t, err)
+
+	assert.JSONEq(t, string(file), rw.Body.String())
+}
+
+func TestHandlers_List_GetByName_hidden(t *testing.T) {
+	data := db.Plugin{
+		Author:        "traefik",
+		Compatibility: "v2",
+		CreatedAt:     time.Date(2020, 1, 1, 1, 0, 0, 0, time.UTC),
+		DisplayName:   "Demo Plugin",
+		ID:            "276809780784267776",
+		Import:        "github.com/traefik/plugindemo",
+		LatestVersion: "v0.2.1",
+		Name:          "github.com/traefik/plugindemo",
+		Readme:        "README",
+		Snippet:       map[string]interface{}{"toml": "toml", "yaml": "yaml"},
+		Stars:         22,
+		Summary:       "[Demo] Add Request Header",
+		Type:          "middleware",
+		Versions:      []string{"v0.2.1", "v0.2.0", "v0.1.0"},
+		Hidden:        true,
+	}
+
+	testDB := mockDB{
+		getByNameFn: func(_ context.Context, _ string, filterHidden bool) (db.Plugin, error) {
+			if filterHidden {
+				return data, nil
+			}
+			return db.Plugin{}, db.NotFoundError{}
+		},
+	}
+
+	rw := httptest.NewRecorder()
+
+	req := httptest.NewRequest(http.MethodGet, "/?name=Demo%20Plugin", http.NoBody)
+
+	New(testDB, nil, nil).getByName(rw, req)
+
+	assert.Equal(t, http.StatusNotFound, rw.Code)
+
+	rw = httptest.NewRecorder()
+
+	req = httptest.NewRequest(http.MethodGet, "/?name=Demo%20Plugin&filterHidden=true", http.NoBody)
+
+	New(testDB, nil, nil).getByName(rw, req)
+
+	assert.Equal(t, http.StatusOK, rw.Code)
+
+	file, err := os.ReadFile("./fixtures/get_plugin_by_name_hidden.json")
 	require.NoError(t, err)
 
 	assert.JSONEq(t, string(file), rw.Body.String())
@@ -127,7 +177,7 @@ func TestHandlers_List_SearchByName(t *testing.T) {
 	}
 
 	testDB := mockDB{
-		getByNameFn: func(_ context.Context, _ string) (db.Plugin, error) {
+		getByNameFn: func(_ context.Context, _ string, _ bool) (db.Plugin, error) {
 			return data, nil
 		},
 	}
