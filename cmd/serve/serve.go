979146151fe9f7e7b141c08397a6ca1c6e39742c
Romain <rtribotte@users.noreply.github.com>
2025-07-31 15:40:04 +0200

    Disallow download of un-verified versions for WASM plugins

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/google/go-github/v57/github"
+	"github.com/google/go-github/v74/github"
 	"github.com/gorilla/mux"
 	"github.com/julienschmidt/httprouter"
 	"github.com/ldez/grignotin/goproxy"
